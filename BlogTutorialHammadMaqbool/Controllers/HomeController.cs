b940d576e1cd0d54a4543fba8e696a29b8a35e5b
sherkhanbaloch <sherbalochcs@gmail.com>
2025-01-25 21:59:05 +0500

    route to single post after clicking on read more link.

--- BlogTutorialHammadMaqbool/Controllers/HomeController.cs
+++ BlogTutorialHammadMaqbool/Controllers/HomeController.cs
@@ -1,17 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BlogTutorialHammadMaqbool.Data;
+using BlogTutorialHammadMaqbool.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BlogTutorialHammadMaqbool.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly AppDbContext db;
+
+        public HomeController(AppDbContext _db)
+        {
+            db = _db;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Post> myPost = db.Tbl_Post;
+            return View(myPost);
         }
-        
-        public IActionResult Post()
+
+        [Route("Home/Post/{Slug}")]
+        public IActionResult Post(string Slug)
         {
-            return View();
+            var SinglePost = db.Tbl_Post.Where(x => x.Slug == Slug).FirstOrDefault();
+            return View(SinglePost);
         }
     }
 }
