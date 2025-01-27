9f7c11f0db38041f508996d35d32102262462e9c
sherkhanbaloch <sherbalochcs@gmail.com>
2025-01-27 18:19:04 +0500

    saved post from admin panel to database.

---
+++ BlogTutorialHammadMaqbool/Controllers/AdminController.cs
@@ -0,0 +1,34 @@
+﻿using BlogTutorialHammadMaqbool.Data;
+using BlogTutorialHammadMaqbool.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogTutorialHammadMaqbool.Controllers
+{
+    public class AdminController : Controller
+    {
+        AppDbContext db;
+
+        public AdminController(AppDbContext _db)
+        {
+            db = _db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult AddPost()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddPost(Post myPost)
+        {
+            db.Tbl_Post.Add(myPost);
+            db.SaveChanges();
+            return View();
+        }
+    }
+}
